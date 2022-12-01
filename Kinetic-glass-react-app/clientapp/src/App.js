4ed26a6ae2be9e3ea71608cad0301ff64298eebf
umarchatha <ahsankarim699@gmail.com>
2022-12-01 19:41:48 +0500

    init

--- Kinetic-glass-react-app/clientapp/src/App.js
+++ Kinetic-glass-react-app/clientapp/src/App.js
@@ -12,32 +12,7 @@ import SellerOrdersPage from './Components/Pages/SellerPages/SellerOrdersPage';
 
  
 function App() {
-  const props= {
-    title: {
-      text: "not mention",
-      class: "",
-    },
-    img: {
-      url: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
-      class: "",
-    },
-    body: {
-      text: "Not Mention",
-      class: "",
-    },
-    header: {
-      text: "Not Mention",
-      class: "",
-    },
-    button: {
-      text: "not given",
-      class: "",
-    },
-    views: {
-      text: "0.0",
-      class: "",
-    },
-  }
+  
   return (
  
     <Router>
